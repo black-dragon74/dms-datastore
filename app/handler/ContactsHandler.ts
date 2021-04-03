0fb61fdfca1be0e2a730f5b7b7fded8dba572f1d
black.dragon74 <nickk.2974@gmail.com>
2021-04-03 20:45:09 +0530

    Fix phone number formatting
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

--- app/handler/ContactsHandler.ts
+++ app/handler/ContactsHandler.ts
@@ -16,7 +16,7 @@ interface FacultyContact {
 
 export default function ContactsHandler(req: express.Request, res: express.Response) {
     // Get the Faculty details page form the uni website
-    axios.get(CONST.CONTACTS_URL).then((response) => {
+    axios.get(CONST.CONTACTS_URL).then(response => {
         // Create a new cheerio object from the response
         const $ = cheerio.load(response.data.toString())
 
@@ -24,7 +24,7 @@ export default function ContactsHandler(req: express.Request, res: express.Respo
         const respData = $(CONST.CONTACT_FIELD_ID).attr('data-faculty-object')
         if (respData == undefined) {
             console.error("Unable to find the faculty data object")
-            // res.send(500)
+            res.send(500).send(errorToJSON("Unable to find the faculty data object"))
             return
         }
 
@@ -33,6 +33,7 @@ export default function ContactsHandler(req: express.Request, res: express.Respo
 
         // Loop and create objects
         let jsonResponse = new Array<FacultyContact>()
+        let id = 1
 
         for (let i = 0; i < jsonData.length; i++) {
             let currData = jsonData[i]
@@ -41,26 +42,31 @@ export default function ContactsHandler(req: express.Request, res: express.Respo
             if (name.length == 0)
                 continue
 
+            let phone = currData["phone"] as String
+            phone = phone.trim()
+                .split(',')[0]
+                .replace('+91', '')
+                .replace(' ', '')
+                .replace('-', '')
+
             let currFac: FacultyContact = {
                 department: currData["departmentText"],
                 designation: currData["designation"],
                 email: currData["email"],
-                id: i.toString(),
+                id: id.toString(),
                 image: CONST.UNI_BASE_URL + currData["thumbnailImagePath"],
                 name: name,
-                phone: currData["phone"]
+                phone: phone
             }
 
             jsonResponse.push(currFac)
+            id += 1
         }
 
         res.send(jsonResponse)
 
-    }).catch((error) => {
+    }).catch(error => {
         console.error(error)
+        res.status(500).send(error.toString())
     })
-}
-
-function sanitize(val: String): String {
-    return ""
 }
